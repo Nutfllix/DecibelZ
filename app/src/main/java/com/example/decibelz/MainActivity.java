71f8e49683b62992edda6043bad55873a877d7f9
Kacper <nutfllix@gmail.com>
2025-03-01 20:15:36 +0100

    added asking for recording permission

--- app/src/main/java/com/example/decibelz/MainActivity.java
+++ app/src/main/java/com/example/decibelz/MainActivity.java
@@ -1,17 +1,38 @@
 package com.example.decibelz;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+/////////////////handles permisions
+    private void requestAccessToRecording() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int id, String[] permissions, int[] grant) {
+        super.onRequestPermissionsResult(id, permissions, grant);
+
+        if (id == 1 && grant[0] == PackageManager.PERMISSION_GRANTED ) {
+            //startRecording();
+        } else if (id == 1) {
+            Toast.makeText(this,"This app needs to record audio", Toast.LENGTH_SHORT).show();
+        }
+    }
+////////////////////////////////
 
 
 
@@ -46,5 +67,15 @@ public class MainActivity extends AppCompatActivity {
             stopService(serviceIntent);
         });
         ///////////////////////////////
+
+        ///////////////////////////////
+        //checks for permision
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            //recording allowed so start
+        } else {
+            requestAccessToRecording();
+        }
+        ////////////////////////////////
     }
 }
